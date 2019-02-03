c9b479cf83dc1a6c257414e0fa363ae17a217f55
ASharph <aydar3107@yandex.ru>
2019-02-04 02:22:13 +0300

    Complete

---
+++ GetImage/HtmlManagment.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+
+namespace GetImage
+{
+    class HtmlManagment
+    {
+        public static void CreateHtmlFile(string url, string path)
+        {
+            using (WebClient client = new WebClient()) // WebClient class inherits IDisposable
+            {
+                client.DownloadFile(url, path);
+            }
+        }
+
+        public static List<string> GetLines(string path, string tag)
+        {
+            List<string> lines = new List<string>();
+            string line;
+            using (StreamReader sr = new StreamReader(path))
+            {
+                while ((line = sr.ReadLine()) != null)
+                {
+                    if (line.Contains(tag))
+                    {
+                        lines.Add(line);
+                    }
+                }
+            }
+
+            return lines;
+        }
+
+        public static void CreateImages(List<string> links, string directory)
+        {
+            using (WebClient wb = new WebClient())
+            {
+                for (int i = 0; i < links.Count; i++)
+                {
+                    if (links[i].IndexOf(".png") != -1)
+                    {
+                        wb.DownloadFile(links[i], directory + $"\\{i.ToString()}.png");
+                    }
+
+                    if (links[i].IndexOf(".jpg") != -1)
+                    {
+                        wb.DownloadFile(links[i], directory + $"\\{i.ToString()}.jpg");
+                    }
+                }
+            }
+        }
+
+        public static void GetImgLink(ref List<string> links)
+        {
+            string quote = '\u0022'.ToString();
+
+            for (int i = 0; i < links.Count; i++)
+            {
+                links[i] = GetStringBetween(links[i], "src=" + quote, quote);
+
+                if (links[i].IndexOf("http:") == -1 && links[i].IndexOf("https:") == -1)
+                {
+                    links[i] = "http:" + links[i];
+                }
+            }
+        }
+
+        private static string GetStringBetween(string s, string from, string until)
+        {
+            int startIndex = s.IndexOf(from) + from.Length;
+            int entIndex = s.IndexOf(until, startIndex);
+
+            return s.Substring(startIndex, entIndex - startIndex);
+        }
+    }
+}
--- GetImage/Program.cs
+++ GetImage/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Net;
+using System.IO;
 
 namespace GetImage
 {
@@ -10,6 +12,45 @@ namespace GetImage
     {
         static void Main(string[] args)
         {
-        }
+            string url = Console.ReadLine();
+            string path = @"D:\1\localfile.html";
+
+            Console.WriteLine();
+
+            if (url.IndexOf("http") == -1 && url.IndexOf("https") == -1)
+            {
+                url = @"http://"+url;
+            }
+
+            try
+            {
+                HtmlManagment.CreateHtmlFile(url, path);
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e);
+                return;
+            }
+
+            List<string> tagLines = HtmlManagment.GetLines(path, "<img");
+            HtmlManagment.GetImgLink(ref tagLines);
+            
+            foreach (string s in tagLines)
+            {
+                Console.WriteLine(s);
+            }
+
+            tagLines.RemoveRange(5, tagLines.Count - 5 );
+
+            try
+            {
+                HtmlManagment.CreateImages(tagLines, @"D:\1");
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e);
+                return;
+            }
+        }        
     }
 }
